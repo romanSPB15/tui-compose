558dd2f0644656951683015e469a4401075f8e5f
RomanSPB15 <r@createlec.ru>
2026-06-02 11:55:12 +0300

    Исправлен экран ошибки

--- recovery.go
+++ recovery.go
@@ -9,9 +9,9 @@ func recoveryScreen(message string) {
 	fmt.Fprint(currentApp.f, "\033[2J\033[H")
 	fmt.Fprint(currentApp.f, "\033[44m")
 	w := currentApp.Window().Width()
-	fmt.Fprintf(currentApp.f, fmt.Sprintf("%%-%ds", w), "Ошибка")
-	fmt.Fprintf(currentApp.f, message+"\r\n")
-	fmt.Fprintf(currentApp.f, "Нажмите ENTER для выхода...")
+	fmt.Fprintf(currentApp.f, fmt.Sprintf("%%-%ds", w)+"\r\n", "Ошибка")
+	fmt.Fprintf(currentApp.f, fmt.Sprintf("%%-%ds", w)+"\r\n", message)
+	fmt.Fprintf(currentApp.f, fmt.Sprintf("%%-%ds", w)+"\r\n", "Нажмите ENTER для выхода...")
 	for range currentApp.Window().Height() - 3 {
 		fmt.Println(strings.Repeat(" ", w))
 	}
