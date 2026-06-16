a50fb6071d2cbe15b210fffad22f04cdc21403e0
RomanSPB15 <r@createlec.ru>
2026-06-16 16:06:54 +0300

    Добавлен VBox

---
+++ container.go
@@ -0,0 +1,59 @@
+package tui
+
+type VBox struct {
+	children  []Widget
+	positions []Pos
+	idx       int
+	gap       int
+}
+
+func NewVBox(children ...Widget) *VBox {
+	v := &VBox{gap: 1}
+	v.children = append(v.children, children...)
+	v.layout()
+	return v
+}
+
+func (v *VBox) Add(widgets ...Widget) {
+	v.children = append(v.children, widgets...)
+	v.layout()
+}
+
+func (v *VBox) layout() {
+	v.positions = make([]Pos, len(v.children))
+	line := 0
+	for i := range v.children {
+		v.positions[i] = Pos{Line: line, Col: 0}
+		line += 1 + v.gap
+	}
+}
+
+func (v *VBox) InnerText() string { return "" }
+
+func (v *VBox) MaxLength() int {
+	max := 0
+	for _, child := range v.children {
+		if child.MaxLength() > max {
+			max = child.MaxLength()
+		}
+	}
+	return max
+}
+
+// func (v *VBox) SetIndex(idx int) {
+// 	v.idx = idx
+// 	for _, child := range v.children {
+// 		child.SetIndex(idx)
+// 	}
+// }
+
+func (v *VBox) Child() []Widget {
+	return v.children
+}
+
+func (v *VBox) Pos(i int) Pos {
+	if i < 0 || i >= len(v.positions) {
+		return Pos{}
+	}
+	return v.positions[i]
+}
