4b4fae1a76bb95a782b5227ddcc4c492f7732b3f
RomanSPB15 <r@createlec.ru>
2026-06-24 15:25:16 +0300

    Добавлено дополнение пробелами виджетов

--- core.go
+++ core.go
@@ -104,7 +104,8 @@ func (wnd *window) drawContainer(buf io.Writer, p Pos, c Container) {
 			wnd.drawContainer(buf, pos, c)
 		} else {
 			fmt.Fprintf(buf, "\033[%d;%dH\033[0m", pos.Line+1+p.Line, pos.Col+1+p.Col)
-			fmt.Fprint(buf, w.InnerText())
+			it := w.InnerText()
+			fmt.Fprint(buf, it+strings.Repeat(" ", w.MaxWidth()-len(it)))
 		}
 	}
 }
