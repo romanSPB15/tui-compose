3e577b1d150fe9a922f810c6eca00b78e0579def
RomanSPB15 <r@createlec.ru>
2026-07-28 14:59:12 +0300

    Убран recovery

--- recovery.go
+++ recovery.go
@@ -4,8 +4,6 @@ package tui
 
 import (
 	"fmt"
-	"strings"
-	"time"
 
 	term "github.com/romanSPB15/tui-compose/v3/term"
 )
@@ -17,18 +15,5 @@ func recoveryScreen(message string) {
 	currentWindow.restoreOut()
 	term.Restore()
 
-	fmt.Fprint(currentWindow.f, "\033[3J")
-	fmt.Fprint(currentWindow.f, "\033[44m")
-	time.Sleep(time.Millisecond * 300)
-	wnd := currentWindow.Width()
-	format := fmt.Sprintf("%%-%ds", wnd) + "\r\n"
-	fmt.Fprintf(currentWindow.f, format, "TUI Compose Framework")
-	fmt.Fprintf(currentWindow.f, format, message)
-	fmt.Fprintf(currentWindow.f, format, "Нажмите ENTER для выхода...")
-	for range currentWindow.Height() - 4 {
-		fmt.Fprintln(currentWindow.f, strings.Repeat(" ", wnd))
-	}
-	fmt.Fprint(currentWindow.f, strings.Repeat(" ", wnd))
-	fmt.Fprint(currentWindow.f, "\033[0m")
-	fmt.Scanln()
+	fmt.Fprint(currentWindow.f, message)
 }
