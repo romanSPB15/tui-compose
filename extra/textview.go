7ced8aac28e54df925706548129e7931cb5fb07a
RomanSPB15 <r@createlec.ru>
2026-08-21 15:29:27 +0300

    feat: magenta тег цвета в TextView

--- extra/textview.go
+++ extra/textview.go
@@ -25,47 +25,46 @@ func (tv *TextView) ScrollDown() {
 }
 
 var replaceList = map[string]string{
-	// Обычные цвета текста
 	"fg-black":   "30",
 	"fg-red":     "31",
 	"fg-green":   "32",
 	"fg-yellow":  "33",
 	"fg-blue":    "34",
 	"fg-purpure": "35",
+	"fg-magenta": "35",
 	"fg-cyan":    "36",
 	"fg-white":   "37",
 
-	// Обычные цвета фона
 	"bg-black":   "40",
 	"bg-red":     "41",
 	"bg-green":   "42",
 	"bg-yellow":  "43",
 	"bg-blue":    "44",
 	"bg-purpure": "45",
+	"bg-magenta": "45",
 	"bg-cyan":    "46",
 	"bg-white":   "47",
 
-	// Яркие цвета текста (коды 90-97)
 	"fg-bright-black":   "90",
 	"fg-bright-red":     "91",
 	"fg-bright-green":   "92",
 	"fg-bright-yellow":  "93",
 	"fg-bright-blue":    "94",
 	"fg-bright-purpure": "95",
+	"fg-bright-magenta": "95",
 	"fg-bright-cyan":    "96",
 	"fg-bright-white":   "97",
 
-	// Яркие цвета фона (коды 100-107)
 	"bg-bright-black":   "100",
 	"bg-bright-red":     "101",
 	"bg-bright-green":   "102",
 	"bg-bright-yellow":  "103",
 	"bg-bright-blue":    "104",
 	"bg-bright-purpure": "105",
+	"bg-bright-magenta": "105",
 	"bg-bright-cyan":    "106",
 	"bg-bright-white":   "107",
 
-	// Стили
 	"bold":      "1",
 	"cursive":   "3",
 	"italic":    "3",
